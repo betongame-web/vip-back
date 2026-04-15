f00f79b10fb59de526307643b6a64f0bfdeaa9aa
betongame-web <asimhalder612@gmail.com>
2026-04-16 00:47:27 +0600

    Update SettingController.php

--- app/Http/Controllers/Api/Settings/SettingController.php
+++ app/Http/Controllers/Api/Settings/SettingController.php
@@ -3,63 +3,24 @@
 namespace App\Http\Controllers\Api\Settings;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 
 class SettingController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        return response()->json(['setting' => \Helper::getSetting()], 200);
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        return response()->json([
+            'setting' => [
+                'site_name' => 'ViperPro',
+                'currency_code' => 'USD',
+                'currency_symbol' => '$',
+                'min_deposit' => 10,
+                'max_deposit' => 10000,
+                'min_withdrawal' => 20,
+                'suitpay_is_enable' => true,
+                'stripe_is_enable' => true,
+                'rollover' => 1,
+                'disable_spin' => false,
+            ]
+        ], 200);
     }
 }
