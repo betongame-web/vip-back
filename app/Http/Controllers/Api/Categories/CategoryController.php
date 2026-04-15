ef8ca32806dc62292312ce9b26c61f581705203f
betongame-web <asimhalder612@gmail.com>
2026-04-16 00:51:10 +0600

    Update CategoryController.php

--- app/Http/Controllers/Api/Categories/CategoryController.php
+++ app/Http/Controllers/Api/Categories/CategoryController.php
@@ -3,65 +3,32 @@
 namespace App\Http\Controllers\Api\Categories;
 
 use App\Http\Controllers\Controller;
-use App\Models\Category;
-use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        $categories = Category::get();
-        return response()->json(['categories' => $categories]);
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        return response()->json([
+            'categories' => [
+                [
+                    'id' => 1,
+                    'name' => 'Slots',
+                    'slug' => 'slots',
+                    'image' => '/assets/images/wager_1_6ec39cf4.png',
+                ],
+                [
+                    'id' => 2,
+                    'name' => 'Live Casino',
+                    'slug' => 'live-casino',
+                    'image' => '/assets/images/wager_2_8af53176.png',
+                ],
+                [
+                    'id' => 3,
+                    'name' => 'Crash',
+                    'slug' => 'crash',
+                    'image' => '/assets/images/wager_3_ee25b52f.png',
+                ],
+            ]
+        ], 200);
     }
 }
