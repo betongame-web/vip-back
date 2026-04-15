4f801090da771f20c37588f054831f321122425c
betongame-web <asimhalder612@gmail.com>
2026-04-16 00:47:59 +0600

    Update BannerController.php

--- app/Http/Controllers/Api/Settings/BannerController.php
+++ app/Http/Controllers/Api/Settings/BannerController.php
@@ -3,64 +3,28 @@
 namespace App\Http\Controllers\Api\Settings;
 
 use App\Http\Controllers\Controller;
-use App\Models\Banner;
-use Illuminate\Http\Request;
 
 class BannerController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        return response()->json(['banners' => Banner::get()]);
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        return response()->json([
+            'banners' => [
+                [
+                    'id' => 1,
+                    'title' => 'Main Banner',
+                    'type' => 'carousel',
+                    'image' => '/assets/images/invite_bg_m_bafe1d0e.png',
+                    'link' => '/casinos',
+                ],
+                [
+                    'id' => 2,
+                    'title' => 'Home Banner',
+                    'type' => 'home',
+                    'image' => '/assets/images/invite_bg_m_bafe1d0e.png',
+                    'link' => '/sports',
+                ],
+            ]
+        ], 200);
     }
 }
