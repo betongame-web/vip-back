02a6d7e0c14d24d1ff7c7cc6593a607d3b0d9b64
betongame-web <asimhalder612@gmail.com>
2026-04-16 12:36:43 +0600

    Update BannerController.php

--- app/Http/Controllers/Api/Settings/BannerController.php
+++ app/Http/Controllers/Api/Settings/BannerController.php
@@ -14,14 +14,14 @@ class BannerController extends Controller
                     'id' => 1,
                     'title' => 'Main Banner',
                     'type' => 'carousel',
-                    'image' => '/assets/images/invite_bg_m_bafe1d0e.png',
+                    'image' => url('/assets/images/invite_bg_m_bafe1d0e.png'),
                     'link' => '/casinos',
                 ],
                 [
                     'id' => 2,
                     'title' => 'Home Banner',
                     'type' => 'home',
-                    'image' => '/assets/images/invite_bg_m_bafe1d0e.png',
+                    'image' => url('/assets/images/invite_bg_m_bafe1d0e.png'),
                     'link' => '/sports',
                 ],
             ]
