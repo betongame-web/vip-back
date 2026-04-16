d6cebd3f5f2ac4222d04c7787db534a3a43e6b77
betongame-web <asimhalder612@gmail.com>
2026-04-17 04:04:26 +0600

    Update CategoryController.php

--- app/Http/Controllers/Api/Categories/CategoryController.php
+++ app/Http/Controllers/Api/Categories/CategoryController.php
@@ -14,19 +14,19 @@ class CategoryController extends Controller
                     'id' => 1,
                     'name' => 'Slots',
                     'slug' => 'slots',
-                    'image' => url('/favicon.ico'),
+                    'image' => url('/assets/images/wager_1.6ec39cf4.png'),
                 ],
                 [
                     'id' => 2,
                     'name' => 'Originals',
                     'slug' => 'originals',
-                    'image' => url('/favicon.ico'),
+                    'image' => url('/assets/images/wager_2.8af53176.png'),
                 ],
                 [
                     'id' => 3,
                     'name' => 'Popular',
                     'slug' => 'popular',
-                    'image' => url('/favicon.ico'),
+                    'image' => url('/assets/images/wager_3.ee25b52f.png'),
                 ],
             ]
         ], 200);
