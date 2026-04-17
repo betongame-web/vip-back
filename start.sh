2aeee1372666708ab825c903f6488eb04c1c4c8d
betongame-web <asimhalder612@gmail.com>
2026-04-17 07:52:40 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,19 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 set -e
 
-exec php -S 0.0.0.0:${PORT:-10000} -t public server.php
+cd /app
+
+if [ ! -f .env ]; then
+  cp .env.example .env || true
+fi
+
+php artisan key:generate --force || true
+php artisan config:clear || true
+php artisan route:clear || true
+php artisan view:clear || true
+php artisan cache:clear || true
+php artisan optimize:clear || true
+
+php artisan migrate --force || true
+
+php -S 0.0.0.0:${PORT:-10000} -t public server.php
