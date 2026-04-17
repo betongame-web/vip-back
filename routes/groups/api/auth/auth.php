7ef72e8a92cde41f8b1c6d4483dd4dceaeca9eec
betongame-web <asimhalder612@gmail.com>
2026-04-17 17:26:33 +0600

    Update auth.php

--- routes/groups/api/auth/auth.php
+++ routes/groups/api/auth/auth.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\Api\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => 'api'], function () {
-
     Route::post('login', [AuthController::class, 'login']);
     Route::post('register', [AuthController::class, 'register']);
 
@@ -12,7 +11,7 @@ Route::group(['middleware' => 'api'], function () {
     Route::post('reset-password/{token}', [AuthController::class, 'submitResetPassword']);
 
     Route::group(['middleware' => ['auth.jwt']], function () {
-        Route::get('verify', [AuthController::class, 'verify']); // verifica se está autenticado
+        Route::get('verify', [AuthController::class, 'verify']);
         Route::post('refresh', [AuthController::class, 'refresh']);
         Route::post('logout', [AuthController::class, 'logout']);
         Route::post('me', [AuthController::class, 'me']);
